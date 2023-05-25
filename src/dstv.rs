82b4ca7d9dfa653960b13f9411a0d9136b4afdcc
Thomas Luijken <tluijken@outlook.com>
2023-05-25 22:47:52 +0200

    Pattern matching

--- src/dstv.rs
+++ src/dstv.rs
@@ -18,13 +18,10 @@ impl Dstv {
             .fold(Vec::<(ElementType, Vec<&str>)>::new(), |mut acc, line| {
                 // get the element type from the first two characters
                 let element_type = ElementType::from_str(&line[0..2]);
-                if element_type != ElementType::None && element_type != ElementType::EndOfFile {
-                    // if the first two characters represent an element type, start a new element
-                    // block
-                    acc.push((element_type, Vec::<&str>::new()));
-                } else {
-                    // if the first two characters are not a valid element type, then it is a continuation of the previous element
-                    acc.last_mut().unwrap().1.push(line);
+                match element_type {
+                    ElementType::None => acc.last_mut().unwrap().1.push(line),
+                    ElementType::EndOfFile => {}
+                    _ => acc.push((element_type, vec![line])),
                 }
                 acc
             });
